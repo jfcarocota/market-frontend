767f53322b6e5a7d54223e582063ab3de3db3a89
jfcarocota <jfcarocota@gmail.com>
2020-09-30 20:02:28 -0700

    Updates

---
+++ precache-manifest.f2a4b36ff3a9e10bb4b236ef4431cbd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef938d978af4e4c469d00458af2080a",
+    "url": "/market-frontend/index.html"
+  },
+  {
+    "revision": "5adb9bf4eb090fb9f79e",
+    "url": "/market-frontend/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "075d689d209cc08d3065",
+    "url": "/market-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5adb9bf4eb090fb9f79e",
+    "url": "/market-frontend/static/js/2.60edea4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/market-frontend/static/js/2.60edea4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "075d689d209cc08d3065",
+    "url": "/market-frontend/static/js/main.97e51d59.chunk.js"
+  },
+  {
+    "revision": "f08eb1cf9a4f1b0d550d",
+    "url": "/market-frontend/static/js/runtime-main.acd33615.js"
+  },
+  {
+    "revision": "2aaf256dd9a40e397c43abddc97e81b1",
+    "url": "/market-frontend/static/media/mllogo.2aaf256d.png"
+  }
+]);
